e3ebc69898d4c19e3e29be016edb6ca595d74929
iamabigartist <53459343+iamabigartist@users.noreply.github.com>
2022-03-03 15:09:06 +0800

    string separator

--- Assets/PrototypeUtils/Runtime/EnumerateUtility.cs
+++ Assets/PrototypeUtils/Runtime/EnumerateUtility.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using UnityEngine;
 namespace MUtility
 {
     public static class EnumerateUtility
@@ -11,69 +10,76 @@ namespace MUtility
         public static T[] Copy<T>(this T[] array)
         {
             var new_array = new T[array.Length];
-            Array.Copy(array, new_array, 0);
+            Array.Copy( array, new_array, 0 );
+
             return new_array;
         }
 
-        public static string ToMString<T>(this IEnumerable<T> array)
+        public static string ToMString<T>(this IEnumerable<T> array, string separator)
         {
-            return string.Join(",", array);
+            return string.Join( separator, array );
         }
 
-        #region SQL
+    #region SQL
 
         public static int[] AllIndexOf<T>(this T[] source, T value)
         {
             var l = new List<int>();
+
             for (int i = 0; i < source.Length; i++)
             {
-                if (source[i].Equals(value)) { l.Add(i); }
+                if (source[i].Equals( value )) { l.Add( i ); }
             }
+
             return l.ToArray();
         }
 
-        #endregion
+    #endregion
 
-        #region Range
+    #region Range
 
         public static int[] ToArray(this Range r)
         {
-            return Enumerable.Range(r.Start.Value, r.End.Value - r.Start.Value).ToArray();
+            return Enumerable.Range( r.Start.Value, r.End.Value - r.Start.Value ).ToArray();
         }
 
-        #endregion
+    #endregion
 
-        #region LinkedList
+    #region LinkedList
 
         public static LinkedListNode<T> AppendLast<T>(this LinkedList<T> list, T[] array)
         {
-            var head = list.AddLast(array[0]);
+            var head = list.AddLast( array[0] );
+
             for (int i = 1; i < array.Length; i++)
             {
-                list.AddLast(array[i]);
+                list.AddLast( array[i] );
             }
+
             return head;
         }
 
-        #endregion
+    #endregion
 
-        #region Flatten
+    #region Flatten
 
         public static int FlattenedCount<T>(this IEnumerable<IEnumerable<T>> lists)
         {
-            return lists.Sum(list => list.Count());
+            return lists.Sum( list => list.Count() );
         }
 
         public static T[] ToFlattenedArray<T>(this LinkedList<List<T>> lists)
         {
             var array = new T[lists.FlattenedCount()];
             int start_index = 0;
+
             for (var i = lists.First; i != null; i = i.Next)
             {
                 var cur_list = i.Value;
-                cur_list.CopyTo(array, start_index);
+                cur_list.CopyTo( array, start_index );
                 start_index = +cur_list.Count;
             }
+
             return array;
         }
 
@@ -81,11 +87,12 @@ namespace MUtility
         {
             var array = new T[lists.FlattenedCount()];
 
-            Parallel.For(0, lists.Count, i =>
-           {
-               var cur_list = lists[i];
-               cur_list.CopyTo(array, start_indices[i]);
-           });
+            Parallel.For( 0, lists.Count, i =>
+            {
+                var cur_list = lists[i];
+                cur_list.CopyTo( array, start_indices[i] );
+            } );
+
             return array;
         }
 
@@ -93,11 +100,11 @@ namespace MUtility
         {
             var array = new T[lists.FlattenedCount()];
 
-            Parallel.For(0, lists.Count, i =>
-           {
-               var cur_list = lists[i];
-               cur_list.CopyTo(array, start_indices[i]);
-           });
+            Parallel.For( 0, lists.Count, i =>
+            {
+                var cur_list = lists[i];
+                cur_list.CopyTo( array, start_indices[i] );
+            } );
 
             return array;
         }
@@ -107,12 +114,14 @@ namespace MUtility
             var copy_positions = new int[arrays.Count()];
             int i = 0;
             int sum = 0;
+
             foreach (var array in arrays)
             {
                 copy_positions[i] = sum;
                 i++;
                 sum += array.Count();
             }
+
             return copy_positions;
         }
 
@@ -137,15 +146,16 @@ namespace MUtility
             var flatten_array = new T[write_positions.Last()];
 
             int i = 0;
+
             foreach (var array in arrays)
             {
-                array.ToArray().CopyTo(flatten_array, write_positions[i]);
+                array.ToArray().CopyTo( flatten_array, write_positions[i] );
                 i++;
             }
 
             return flatten_array;
         }
 
-        #endregion
+    #endregion
     }
 }
