7d0fca7634f2d8c2b16953d548daaea029461b01
iamabigartist <53459343+iamabigartist@users.noreply.github.com>
2022-03-07 19:20:40 +0800

    Update EnumerateUtility.cs

--- Assets/PrototypeUtils/Runtime/EnumerateUtility.cs
+++ Assets/PrototypeUtils/Runtime/EnumerateUtility.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-namespace MUtility
+namespace PrototypeUtils
 {
     public static class EnumerateUtility
     {
