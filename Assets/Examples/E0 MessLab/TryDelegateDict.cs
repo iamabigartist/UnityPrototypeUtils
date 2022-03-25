45927f479893d1ff5d2209d329ba79f191d3aa15
iamabigartist <pch4178@gmail.com>
2022-03-25 23:50:46 +0800

    roughly finish statemachine, into testing story

---
+++ Assets/Examples/E0 MessLab/TryDelegateDict.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Examples.E0_MessLab
+{
+    public class TryDelegateDict : MonoBehaviour
+    {
+        public event Action act1;
+        public event Action act2;
+        Dictionary<MulticastDelegate, MulticastDelegate> table;
+
+
+        void BindActs()
+        {
+        }
+
+        void Start()
+        {
+            table = new();
+        }
+
+
+
+    }
+}
--- Assets/PrototypeUtils/Runtime/StateMachine0/StateMachine.cs
+++ Assets/PrototypeUtils/Runtime/StateMachine0/StateMachine.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
 namespace PrototypeUtils.StateMachine0
 {
 
@@ -132,22 +134,254 @@ namespace PrototypeUtils.StateMachine0
     // }
 
 #endregion
-#region InterfaceBased
+#region New
 
-    public interface IMachineShell { }
-    public interface IMachine<TIMachineShell>
-        where TIMachineShell : IMachineShell { }
-    public interface IState<TIMachineShell> : IMachine<TIMachineShell>
-        where TIMachineShell : IMachineShell { }
+    // public abstract class StateMachine { }
+    //
+    // public abstract class StateMachine<TShellMachine> : StateMachine
+    //     where TShellMachine : StateMachine { }
+    //
+    // public abstract class StateMachine<TShellMachine, TLowerMachine> : StateMachine<TShellMachine>
+    //     where TShellMachine : StateMachine
+    //     where TLowerMachine : StateMachine<TShellMachine>
+    // {
+    //     protected StateManager<TShellMachine> m_state_manager;
+    //     protected TShellMachine m_shell;
+    //     protected TLowerMachine m_lower_machine;
+    //
+    //     public void AssignShell(TShellMachine shell)
+    //     {
+    //         m_shell = shell;
+    //     }
+    //     public void AssignLowerMachine(TLowerMachine lower_machine)
+    //     {
+    //         m_lower_machine = lower_machine;
+    //     }
+    //
+    //     public abstract void OnEnable();
+    //     public abstract void OnDisable();
+    //     public abstract void OnUpdate();
+    //
+    // }
+    //
+    // public class StateManager<TShellMachine>
+    //     where TShellMachine : StateMachine
+    // {
+    //     protected StateMachine<TShellMachine, StateMachine<TShellMachine>>[] States;
+    //     protected StateMachine<TShellMachine, StateMachine<TShellMachine>> CurrentState;
+    //     public void AssignShell(TShellMachine shell)
+    //     {
+    //         foreach (var state in States)
+    //         {
+    //             state.AssignShell( shell );
+    //         }
+    //     }
+    //     public void AssignLowerMachine(StateMachine<TShellMachine> current_machine)
+    //     {
+    //         foreach (var state in States)
+    //         {
+    //             state.AssignLowerMachine( current_machine );
+    //         }
+    //     }
+    //
+    // }
+#region Onelevel
+
+//     public abstract class StateMachine
+// {
+// }
+//
+//     public abstract class StateMachine<TShellMachine> : StateMachine
+//         where TShellMachine : StateMachine
+//     {
+//         protected StateManager<TShellMachine> m_state_manager;
+//         protected TShellMachine m_shell;
+//
+//         public void AssignShell(TShellMachine shell)
+//         {
+//             m_shell = shell;
+//         }
+//
+//         public abstract void OnEnable();
+//         public abstract void OnDisable();
+//         public abstract void OnUpdate();
+//     }
+//
+//     public class StateManager<TShellMachine>
+//         where TShellMachine : StateMachine
+//     {
+//         protected StateMachine<TShellMachine>[] States;
+//         protected StateMachine<TShellMachine> CurrentState;
+//         public void AssignShell(TShellMachine shell)
+//         {
+//             foreach (var state in States)
+//             {
+//                 state.AssignShell( shell );
+//             }
+//         }
+//
+//     }
+
+#endregion
+#region Node
+
+    public abstract class Machine
+    {
+        public abstract void OnMachineEnter(string last_state_name);
+        public abstract void OnMachineExit(string next_state_name);
+        public abstract void OnMachineUpdate();
+    }
+    public abstract class NoneMachine : Machine { }
+
+    /// <summary>
+    ///     A machine used as a state of other machines
+    /// </summary>
+    /// <typeparam name="TStatedMachine">The controlled machine that use this machine as a state</typeparam>
+    public abstract class StateMachine<TStatedMachine> : Machine
+        where TStatedMachine : Machine
+    {
+        public TStatedMachine m_stated_machine;
+        public virtual void OnStateEnter(string last_state_name)
+        {
+            OnMachineEnter( last_state_name );
+        }
+        public virtual void OnStateExit(string next_state_name)
+        {
+            OnMachineExit( next_state_name );
+        }
+        public virtual void OnStateUpdate()
+        {
+            OnMachineUpdate();
+        }
+    }
+
+    /// <summary>
+    ///     <para>From <see cref="StateMachine{TStatedMachine}" />: <inheritdoc cref="StateMachine{TStatedMachine}" /></para>
+    ///     <para>At the same time, a machine that use other machine as states.</para>
+    /// </summary>
+    /// <typeparam name="TCurrentMachine">
+    ///     The real type of this machine, used to tell the states what type of machine they are controlling.
+    /// </typeparam>
+    /// <typeparam name="TStatedMachine">
+    ///     <inheritdoc cref="StateMachine" />
+    /// </typeparam>
+    public abstract class StatedStateMachine<TCurrentMachine, TStatedMachine> : StateMachine<TStatedMachine>
+        where TStatedMachine : Machine
+        where TCurrentMachine : StatedStateMachine<TCurrentMachine, TStatedMachine>
+    {
+        public Dictionary<string, StateMachine<TCurrentMachine>> m_states;
+        public string current_state_name;
+
+        protected StatedStateMachine(Dictionary<string, StateMachine<TCurrentMachine>> states)
+        {
+            m_states = states;
+            current_state_name = string.Empty;
+
+            foreach (var state in m_states.Values)
+            {
+                state.m_stated_machine = this as TCurrentMachine;
+            }
+        }
+
+        public void TranslateState(string to_state_name)
+        {
+            m_states[current_state_name].OnStateExit( to_state_name );
+            m_states[to_state_name].OnStateEnter( current_state_name );
+            current_state_name = to_state_name;
+        }
+
+        public sealed override void OnStateEnter(string last_state_name)
+        {
+            m_states[current_state_name].OnStateEnter( string.Empty );
+            OnMachineEnter( last_state_name );
+        }
+
+        public sealed override void OnStateExit(string next_state_name)
+        {
+            m_states[current_state_name].OnStateExit( string.Empty );
+            OnMachineExit( next_state_name );
+        }
+
+        public sealed override void OnStateUpdate()
+        {
+            m_states[current_state_name].OnStateUpdate();
+            OnMachineUpdate();
+        }
+
+    }
+
+#endregion
 
 #region Example
 
-    public interface ILogShell : IMachineShell
+    public class AppleStory : StatedStateMachine<AppleStory, NoneMachine>
     {
-        void UpdateLog();
-        void AddElement();
-        void ResetElement();
-        event Action<string> Log;
+        public string story_text;
+        public event Action<string> DrawStoryTextGUI;
+        public event Func<string, bool> DrawButton;
+        public event Action DrawDogStatesViewer;
+        public event Action<int> GiveTheMountainApples;
+        public AppleStory() : base( new()
+        {
+            { "Chapter1", new Story1() },
+            { "Chapter2", new Story2() }
+        } ) { }
+        public override void OnMachineEnter(string last_state_name) { }
+        public override void OnMachineExit(string next_state_name) { }
+        public override void OnMachineUpdate()
+        {
+            DrawStoryTextGUI( story_text );
+            DrawDogStatesViewer();
+            EditorGUILayout.BeginHorizontal();
+            var turn_back = DrawButton( "<" );
+            var turn_next = DrawButton( ">" );
+            EditorGUILayout.EndHorizontal();
+        }
+    }
+
+    public class Story1 : StatedStateMachine<Story1, AppleStory>
+    {
+        public string story_1_summary;
+        public string story_1_detail;
+        public Story1() : base( new()
+        {
+            { "Start", new Story1_1() },
+            { "End", new Story1_2() }
+        } ) { }
+        public override void OnMachineEnter(string last_state_name) { throw new NotImplementedException(); }
+        public override void OnMachineExit(string next_state_name) { throw new NotImplementedException(); }
+        public override void OnMachineUpdate()
+        {
+            m_stated_machine.story_text = $"\t{story_1_summary}\n\n\t{story_1_detail}";
+        }
+    }
+
+    public class Story1_1 : StateMachine<Story1>
+    {
+        public string story_1_1_detail;
+        public override void OnMachineEnter(string last_state_name) { }
+        public override void OnMachineExit(string next_state_name) { }
+        public override void OnMachineUpdate()
+        {
+            m_stated_machine.story_1_detail = story_1_1_detail;
+        }
+    }
+    public class Story1_2 : StateMachine<Story1>
+    {
+        public string story_1_2_detail;
+        public override void OnMachineEnter(string last_state_name) { }
+        public override void OnMachineExit(string next_state_name) { }
+        public override void OnMachineUpdate()
+        {
+            m_stated_machine.story_1_detail = story_1_2_detail;
+        }
+    }
+
+    public class Story2 : StateMachine<AppleStory>
+    {
+        public override void OnMachineEnter(string last_state_name) { }
+        public override void OnMachineExit(string next_state_name) { }
+        public override void OnMachineUpdate() { }
     }
 
 #endregion
