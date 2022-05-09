0dd0329a6e7b043e8ae4c840fed3db30c5ded975
iamabigartist <pch4178@gmail.com>
2022-05-10 01:45:07 +0800

    finish bone plating
    change init func to manually

--- Assets/Examples/E6 TestSM3/BonePlating.cs
+++ Assets/Examples/E6 TestSM3/BonePlating.cs
@@ -2,13 +2,24 @@
 using PrototypeUtils.StateMachine0.SM3;
 namespace Examples.E6_TestSM3
 {
-	public class BonePlating :
+	public class Damage
+	{
+		public float value;
+		public Damage(float value)
+		{
+			this.value = value;
+		}
+	}
+
+	public partial class BonePlating :
 		StateMachine<BonePlating, BonePlating.DamageModifier, BonePlating.State>
 	{
-		public int max_plating_count = 3;
 		public PeriodTimer activated_duration_timer;
 		public PeriodTimer cooldown_timer;
-		public int cur_plate_count;
+		int max_plating_count;
+		int cur_plate_count;
+		float damage_reduce_value;
+
 		public enum DamageModifier
 		{
 			OnReceivedDamage
@@ -19,27 +30,42 @@ namespace Examples.E6_TestSM3
 			Activated,
 			Cooling
 		}
-		public BonePlating(double activated_duration_ms, double cooldown_ms) : base(
-			new()
-			{
-				{ DamageModifier.OnReceivedDamage, _ => {} }
-			},
-			new()
-			{
-				{ State.Cooled, new Cooled() },
-				{ State.Activated, new Activated() },
-				{ State.Cooling, new Cooling() }
-			}, State.Cooled)
+		public BonePlating(double activated_duration_ms, double cooldown_ms, int max_plating_count, float damage_reduce_value)
 		{
+			InitMachine(
+				new()
+				{
+					{ DamageModifier.OnReceivedDamage, _ => {} }
+				},
+				new()
+				{
+					{ State.Cooled, new Cooled() },
+					{ State.Activated, new Activated() },
+					{ State.Cooling, new Cooling() }
+				}, State.Cooled);
 			activated_duration_timer = new(activated_duration_ms);
 			cooldown_timer = new(cooldown_ms);
-			cur_plate_count = max_plating_count;
+			this.max_plating_count = max_plating_count;
+			cur_plate_count = this.max_plating_count;
+			this.damage_reduce_value = damage_reduce_value;
 		}
 		protected override void OnStateTransition(State new_state) {}
 		public override void OnMachineUpdate()
 		{
 
 		}
+
+	#region Process
+
+		float GetReducedDamage(Damage source_damage) { return source_damage.value - damage_reduce_value; }
+		void UseOnePlate(object param)
+		{
+			if (!(param is (Damage source_damage, Damage result_damage))) { return; }
+			cur_plate_count--;
+			result_damage.value = GetReducedDamage(source_damage);
+		}
+
+	#endregion
 
 	#region Interface
 
@@ -56,6 +82,8 @@ namespace Examples.E6_TestSM3
 		}
 
 		public int CurPlateCount => cur_plate_count;
+		public int MaxPlatingCount { get => max_plating_count; set => max_plating_count = value; }
+		public float DamageReduceValue { get => damage_reduce_value; set => damage_reduce_value = value; }
 
 	#endregion
 
@@ -64,42 +92,106 @@ namespace Examples.E6_TestSM3
 
 		public class Cooled : MachineDriver<BonePlating, DamageModifier, State>
 		{
-			public Cooled() : base(new()
+
+			bool need_activate;
+			public Cooled()
 			{
-				{ DamageModifier.OnReceivedDamage, _ => {} }
-			})
-			{
+				InitDriver(new()
+				{
+					{ DamageModifier.OnReceivedDamage, OnReceivedDamage }
+				});
+				need_activate = false;
 			}
 
-			void OnReceivedDamage()
+			void OnReceivedDamage(object param)
 			{
-				machine_data.cur_plate_count--;
+				need_activate = true;
+				machine.UseOnePlate(param);
 			}
 
 			public override bool NextState(out State next_state)
 			{
-
+				if (need_activate)
+				{
+					next_state = State.Activated;
+					need_activate = false;
+					return true;
+				}
+				else
+				{
+					next_state = default;
+					return false;
+				}
 			}
 		}
 		public class Activated : MachineDriver<BonePlating, DamageModifier, State>
 		{
-			public Activated() : base(new()
+			public Activated()
 			{
-				{ DamageModifier.OnReceivedDamage, _ => {} }
-			})
-			{
+				InitDriver(new()
+				{
+					{ DamageModifier.OnReceivedDamage, OnReceivedDamage }
+				});
 			}
-			public override bool NextState(out State next_state) {}
+
+			public override void Enter()
+			{
+				machine.activated_duration_timer.StartNewPeriod();
+			}
+			public override bool NextState(out State next_state)
+			{
+				if (machine.activated_duration_timer.IsTimeUp())
+				{
+					next_state = State.Cooling;
+					return true;
+				}
+				else
+				{
+					next_state = default;
+					return false;
+				}
+			}
+
+			public void OnReceivedDamage(object param)
+			{
+				if (machine.cur_plate_count > 0)
+				{
+					machine.UseOnePlate(param);
+				}
+				else
+				{
+					machine.activated_duration_timer.SetTimeUp();
+				}
+			}
 		}
 		public class Cooling : MachineDriver<BonePlating, DamageModifier, State>
 		{
-			public Cooling() : base(new()
+			public Cooling()
 			{
-				{ DamageModifier.OnReceivedDamage, _ => {} }
-			})
-			{
+				InitDriver(new()
+				{
+					{ DamageModifier.OnReceivedDamage, _ => {} }
+				});
 			}
-			public override bool NextState(out State next_state) {}
+
+			public override void Enter()
+			{
+				machine.cooldown_timer.StartNewPeriod();
+			}
+
+			public override bool NextState(out State next_state)
+			{
+				if (machine.cooldown_timer.IsTimeUp())
+				{
+					next_state = State.Cooled;
+					return true;
+				}
+				else
+				{
+					next_state = default;
+					return false;
+				}
+			}
 		}
 
 	#endregion
--- Assets/PrototypeUtils/Runtime/StateMachine0/SM3/SM3.cs
+++ Assets/PrototypeUtils/Runtime/StateMachine0/SM3/SM3.cs
@@ -7,11 +7,11 @@ namespace PrototypeUtils.StateMachine0.SM3
 		where TInterfaceEnum : Enum
 		where TStateEnum : Enum
 	{
-		public TThisMachine machine_data;
-		public readonly Dictionary<TInterfaceEnum, Action<object>> interfaces;
-		protected MachineDriver(Dictionary<TInterfaceEnum, Action<object>> interfaces)
+		public TThisMachine machine;
+		public Dictionary<TInterfaceEnum, Action<object>> interfaces;
+		protected void InitDriver(Dictionary<TInterfaceEnum, Action<object>> Interfaces)
 		{
-			this.interfaces = interfaces;
+			interfaces = Interfaces;
 		}
 
 		public virtual void Enter() {}
@@ -26,18 +26,20 @@ namespace PrototypeUtils.StateMachine0.SM3
 		where TInterfaceEnum : Enum
 		where TStateEnum : Enum
 	{
-		readonly Dictionary<TStateEnum, MachineDriver<TThisMachine, TInterfaceEnum, TStateEnum>> drivers;
-		readonly Dictionary<TInterfaceEnum, Action<object>> base_interfaces;
+		Dictionary<TStateEnum, MachineDriver<TThisMachine, TInterfaceEnum, TStateEnum>> drivers;
+		Dictionary<TInterfaceEnum, Action<object>> base_interfaces;
 		public TStateEnum cur_state { get; private set; }
 		MachineDriver<TThisMachine, TInterfaceEnum, TStateEnum> cur_driver;
-		protected StateMachine(Dictionary<TInterfaceEnum, Action<object>> base_interfaces, Dictionary<TStateEnum, MachineDriver<TThisMachine, TInterfaceEnum, TStateEnum>> drivers, TStateEnum start_state)
+
+		protected void InitMachine(Dictionary<TInterfaceEnum, Action<object>> baseInterfaces, Dictionary<TStateEnum, MachineDriver<TThisMachine, TInterfaceEnum, TStateEnum>> Drivers, TStateEnum start_state)
 		{
-			this.base_interfaces = base_interfaces;
-			this.drivers = drivers;
+			base_interfaces = baseInterfaces;
+			drivers = Drivers;
 			cur_state = start_state;
-			cur_driver = drivers[cur_state];
-			foreach (var driver in drivers.Values) { driver.machine_data = this as TThisMachine; }
+			cur_driver = Drivers[cur_state];
+			foreach (var driver in Drivers.Values) { driver.machine = this as TThisMachine; }
 		}
+
 		protected abstract void OnStateTransition(TStateEnum new_state);
 
 		void TransitState(TStateEnum new_state)
