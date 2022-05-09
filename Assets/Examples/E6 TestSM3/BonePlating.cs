0748388073a4350cb5cd35c47e3fa60f4a358aa3
iamabigartist <pch4178@gmail.com>
2022-05-10 00:31:09 +0800

    roughly SM3

---
+++ Assets/Examples/E6 TestSM3/BonePlating.cs
@@ -0,0 +1,108 @@
+﻿using PrototypeUtils;
+using PrototypeUtils.StateMachine0.SM3;
+namespace Examples.E6_TestSM3
+{
+	public class BonePlating :
+		StateMachine<BonePlating.Data, BonePlating.DamageModifier, BonePlating.State>
+	{
+		public int max_plating_count = 3;
+		public class Data
+		{
+			public PeriodTimer activated_duration_timer;
+			public PeriodTimer cooldown_timer;
+			public int cur_plate_count;
+		}
+		public enum DamageModifier
+		{
+			OnReceivedDamage
+		}
+		public enum State
+		{
+			Cooled,
+			Activated,
+			Cooling
+		}
+		public BonePlating(double activated_duration_ms, double cooldown_ms) : base(new(),
+			new()
+			{
+				{ DamageModifier.OnReceivedDamage, _ => {} }
+			},
+			new()
+			{
+				{ State.Cooled, new Cooled() },
+				{ State.Activated, new Activated() },
+				{ State.Cooling, new Cooling() }
+			}, State.Cooled)
+		{
+			data.activated_duration_timer = new(activated_duration_ms);
+			data.cooldown_timer = new(cooldown_ms);
+			data.cur_plate_count = max_plating_count;
+		}
+		protected override void OnStateTransition(State new_state) {}
+		public override void OnMachineUpdate()
+		{
+
+		}
+
+	#region Interface
+
+		public double ActivatedDurationMs
+		{
+			get => data.activated_duration_timer.period_ms;
+			set => data.activated_duration_timer.period_ms = value;
+		}
+		public double CooldownMs
+		{
+			get => data.cooldown_timer.period_ms;
+			set => data.cooldown_timer.period_ms = value;
+		}
+
+	#endregion
+
+
+	#region StateDrivers
+
+		public class Cooled : MachineDriver<Data, DamageModifier, State>
+		{
+			public Cooled() : base(new()
+			{
+				{ DamageModifier.OnReceivedDamage, _ => {} }
+			})
+			{
+			}
+
+			void OnReceivedDamage()
+			{
+				machine_data.cur_plate_count--;
+			}
+
+			public override bool NextState(out State next_state)
+			{
+
+			}
+		}
+		public class Activated : MachineDriver<Data, DamageModifier, State>
+		{
+			public Activated() : base(new()
+			{
+				{ DamageModifier.OnReceivedDamage, _ => {} }
+			})
+			{
+			}
+			public override bool NextState(out State next_state) {}
+		}
+		public class Cooling : MachineDriver<Data, DamageModifier, State>
+		{
+			public Cooling() : base(new()
+			{
+				{ DamageModifier.OnReceivedDamage, _ => {} }
+			})
+			{
+			}
+			public override bool NextState(out State next_state) {}
+		}
+
+	#endregion
+
+	}
+}
---
+++ Assets/PrototypeUtils/Runtime/StateMachine0/SM3/SM3.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+namespace PrototypeUtils.StateMachine0.SM3
+{
+	public abstract class MachineDriver<TMachineModel, TInterfaceEnum, TStateEnum>
+		where TMachineModel : class
+		where TInterfaceEnum : Enum
+	{
+		public TMachineModel machine_data;
+		public readonly Dictionary<TInterfaceEnum, Action<object>> interfaces;
+		protected MachineDriver(Dictionary<TInterfaceEnum, Action<object>> interfaces)
+		{
+			this.interfaces = interfaces;
+		}
+
+		public virtual void Enter() {}
+		public virtual void Exit() {}
+		public virtual void OnDriverUpdate() {}
+		public abstract bool NextState(out TStateEnum next_state);
+
+	}
+
+	public abstract class StateMachine<TMachineData, TInterfaceEnum, TStateEnum>
+		where TMachineData : class
+		where TInterfaceEnum : Enum
+		where TStateEnum : Enum
+	{
+		protected TMachineData data;
+		readonly Dictionary<TStateEnum, MachineDriver<TMachineData, TInterfaceEnum, TStateEnum>> drivers;
+		readonly Dictionary<TInterfaceEnum, Action<object>> base_interfaces;
+		public TStateEnum cur_state { get; private set; }
+		MachineDriver<TMachineData, TInterfaceEnum, TStateEnum> cur_driver;
+		protected StateMachine(TMachineData data, Dictionary<TInterfaceEnum, Action<object>> base_interfaces, Dictionary<TStateEnum, MachineDriver<TMachineData, TInterfaceEnum, TStateEnum>> drivers, TStateEnum start_state)
+		{
+			this.data = data;
+			this.base_interfaces = base_interfaces;
+			this.drivers = drivers;
+			cur_state = start_state;
+			cur_driver = drivers[cur_state];
+			foreach (var driver in drivers.Values) { driver.machine_data = data; }
+		}
+		protected abstract void OnStateTransition(TStateEnum new_state);
+
+		void TransitState(TStateEnum new_state)
+		{
+			cur_driver.Exit();
+			OnStateTransition(new_state);
+			cur_state = new_state;
+			cur_driver = drivers[cur_state];
+			cur_driver.Enter();
+		}
+
+		public void Run(TInterfaceEnum interface_name, object args)
+		{
+			cur_driver.interfaces[interface_name](args);
+			base_interfaces[interface_name](args);
+		}
+
+		public abstract void OnMachineUpdate();
+
+		public void Update()
+		{
+			cur_driver.OnDriverUpdate();
+			if (cur_driver.NextState(out var next_state)) { TransitState(next_state); }
+			OnMachineUpdate();
+		}
+	}
+}
