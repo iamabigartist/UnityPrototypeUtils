968e7605f88bcc2466e75da003bba25179e11ad3
Chua <53459343+iamabigartist@users.noreply.github.com>
2024-04-22 01:18:46 +0800

    add test window run

---
+++ Assets/Examples/E13 TestEditorRunner/Editor/TestWidnowRunner.cs
@@ -0,0 +1,33 @@
+using System;
+using PrototypePackages.MainThreadExecutor.Editor;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+public static class TestWindowRunner
+{
+	static LiteEditorWindow wnd;
+	static VisualElement panel;
+	static float start_time;
+	[MenuItem("Examples/TestWindowRunner")]
+	static void ShowExample()
+	{
+		start_time = Time.realtimeSinceStartup;
+		wnd = new("TestWindow", Update);
+		wnd.Show();
+		panel = new();
+		wnd.root.Add(panel);
+		panel.style.width = 200;
+		panel.style.height = 200;
+		panel.style.borderBottomWidth = 20;
+
+	}
+
+	static void Update()
+	{
+		panel.style.borderBottomColor = new Color((float)Math.Sin(Time.realtimeSinceStartup), 0, 0);
+		if (Time.realtimeSinceStartup - start_time > 5)
+		{
+			wnd.Close();
+		}
+	}
+}
